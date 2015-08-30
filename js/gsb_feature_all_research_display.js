fb0f410fa028ce8736386dda7cd2b9fda70f4aaf
Ayuta Padhi <apadhi@stanford.edu>
2015-08-30 12:34:39 -0700

    WP-5868 modified authoring form options to hidden

--- js/gsb_feature_all_research_display.js
+++ js/gsb_feature_all_research_display.js
@@ -1,34 +1,49 @@
-
 (function ($) {
   Drupal.behaviors.gsb_feature_admission_event_display = {
     attach: function (context, settings) {
 
-      if ($('#edit-exposed-filter-field-publication-type-value-expose-to-visitor').prop('checked'))
-      {
-        $('#edit-exposed-field-publication-type-value').attr('disabled', 'true');
+      if ($('#edit-exposed-filter-field-publication-type-value-expose-to-visitor').prop('checked')) {
+        $('.hide_pub').hide();
+        $('div.form-item-exposed-field-publication-type-value').hide();
+
       }
-      $('div.form-item-exposed-filter-field-publication-type-value-expose-to-visitor :input').change(function(e) {
-        if ($('#edit-exposed-field-publication-type-value').prop('disabled'))
-        {
-          $('#edit-exposed-field-publication-type-value').removeAttr('disabled');
+      $('div.form-item-exposed-filter-field-publication-type-value-expose-to-visitor :input').change(function (e) {
+        $('.hide_pub').toggle();
+        $('div.form-item-exposed-field-publication-type-value').toggle();
+      });
+
+      if ($('#edit-exposed-field-centers-research-programs-tid').val() == 'All') {
+        $('div.form-item-exposed-filter-field-circle-topics-tid-expose-to-visitor').hide();
+        $('.hide_topic').hide();
+        $('div.form-item-exposed-field-circle-topics-tid').hide();
+      }
+      $('div.form-item-exposed-field-centers-research-programs-tid :input').change(function (e) {
+        if ($('#edit-exposed-field-centers-research-programs-tid').val() == 'All') {
+          $('div.form-item-exposed-filter-field-circle-topics-tid-expose-to-visitor').hide();
+          $('.hide_topic').hide();
+          $('div.form-item-exposed-field-circle-topics-tid').hide();
         }
         else {
-          $('#edit-exposed-field-publication-type-value').attr('disabled', 'true');
+          $('div.form-item-exposed-filter-field-circle-topics-tid-expose-to-visitor').show();
+          if ($('#edit-exposed-filter-field-circle-topics-tid-expose-to-visitor').prop('checked')) {
+            $('.hide_topic').hide();
+            $('div.form-item-exposed-field-circle-topics-tid').hide();
+          } else {
+            $('.hide_topic').show();
+            $('div.form-item-exposed-field-circle-topics-tid').show();
+          }
         }
       });
 
-      if ($('#edit-exposed-filter-field-circle-topics-tid-expose-to-visitor').prop('checked'))
-      {
-        $('#edit-exposed-field-circle-topics-tid').attr('disabled', 'true');
+      if ($('#edit-exposed-filter-field-circle-topics-tid-expose-to-visitor').prop('checked')) {
+        $('.hide_topic').hide();
+        $('div.form-item-exposed-field-circle-topics-tid').hide();
       }
-      $('div.form-item-exposed-filter-field-circle-topics-tid-expose-to-visitor :input').change(function(e) {
-        if ($('#edit-exposed-field-circle-topics-tid').prop('disabled'))
-        {
-          $('#edit-exposed-field-circle-topics-tid').removeAttr('disabled');
-        }
-        else {
-          $('#edit-exposed-field-circle-topics-tid').attr('disabled', 'true');
-        }
+      $('div.form-item-exposed-filter-field-circle-topics-tid-expose-to-visitor :input').change(function (e) {
+        $('.hide_topic').toggle();
+        $('div.form-item-exposed-field-circle-topics-tid').toggle();
       });
+    }
   };
 })(jQuery);
+
