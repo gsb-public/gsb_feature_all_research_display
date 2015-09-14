1a1946770e9222eb0376c5ba6b2796e5ba02f8a3
Ayuta Padhi <apadhi@stanford.edu>
2015-09-14 16:05:05 -0700

    WP-5982 adding css class to only publication form.

--- js/gsb_align_exposed_markup.js
+++ js/gsb_align_exposed_markup.js
@@ -1,11 +1,15 @@
 (function ($) {
   Drupal.behaviors.gsb_feature_admission_event_display = {
     attach: function (context, settings) {
-      $('#edit-narrow-search').wrap('<div class="narrow-search-wrapper"/>')
-      $fieldset = $('.narrow-search-wrapper').detach();
-      $fieldset.insertAfter('.views-submit-button');
-      $submitbutton = $('.views-submit-button').detach();
-      $submitbutton.insertAfter('.form-item-field-search-field-value');
+      //
+        $('#edit-narrow-search').wrap('<div class="narrow-search-wrapper"/>');
+        $fieldset = $('.narrow-search-wrapper').detach();
+        $fieldset.insertAfter('#views-exposed-form-gsb-all-research-listing-all-research-panel-pane .views-submit-button');
+
+        if ($('.views-submit-button').length == 1) {
+          $submitbutton = $('.views-submit-button').detach();
+          $submitbutton.insertAfter('.form-item-field-search-field-value');
+        }
     }
   }
 })(jQuery);
